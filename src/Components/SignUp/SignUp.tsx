cd462ecbfc4a1a62354dc5d1d345933bd4a56079
samkeleN <samkelendzululeka@gmail.com>
2024-08-14 01:01:40 +0200

    chore: Add SignUp component with styling and functionality
    Refactor SignUp component to include CSS styling and sign-up functionality for Facebook and Google.

--- src/Components/SignUp/SignUp.tsx
+++ src/Components/SignUp/SignUp.tsx
@@ -1,7 +1,32 @@
+import "./SignUp.css";
+
 const SignUp = () => {
+  const handleFacebookSignUp = () => {
+    // Implement Facebook sign-up logic here
+    console.log("Sign up with Facebook");
+  };
+
+  const handleGoogleSignUp = () => {
+    // Implement Google sign-up logic here
+    console.log("Sign up with Google");
+  };
+
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className="signup-container">
+      <div className="container">
+        <h1>Sign Up</h1>
+        <div className="signup-options">
+          <button
+            className="signup-button facebook"
+            onClick={handleFacebookSignUp}
+          >
+            Sign Up with Facebook
+          </button>
+          <button className="signup-button google" onClick={handleGoogleSignUp}>
+            Sign Up with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
