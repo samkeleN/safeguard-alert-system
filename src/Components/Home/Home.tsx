726fce11c6a18ac0f20fd3d57f479e47302566b7
Samkele Ndzululeka <159358443+samkeleN@users.noreply.github.com>
2024-09-18 16:21:52 +0200

    feat: Add Home component with styling and content

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,7 +1,12 @@
+import MapLocation from "../../assets/map location.png"
+import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <h1>homepage</h1>
+      <div className="home">
+      <h1>Welcome to SafeGuard Alert System</h1>
+      <img src={MapLocation} alt="Map Location" />
+      </div>
     </div>
   );
 };
