ff472c7108960c3fea1c954dc5af67d617b0b11d
Samkele Ndzululeka <159358443+samkeleN@users.noreply.github.com>
2024-09-20 01:54:17 +0200

    feat: Add Firebase configuration and dependencies

---
+++ Firebase.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAP-6_D-3NzPN65dWDbTWKnPPj4YzJN4Oc",
+  authDomain: "safeguard-alert-system.firebaseapp.com",
+  databaseURL: "https://safeguard-alert-system-default-rtdb.firebaseio.com",
+  projectId: "safeguard-alert-system",
+  storageBucket: "safeguard-alert-system.appspot.com",
+  messagingSenderId: "417348098090",
+  appId: "1:417348098090:web:af2d778ad8cf9a6064cb53",
+  measurementId: "G-68DQYMVFLM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default {app, analytics}
